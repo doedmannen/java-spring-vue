31887aa4740a9b067b5ea303fc2850ec79234a2a
Kristoffer <doedmannen@aetmej.se>
2019-04-10 15:45:41 +0200

    init

---
+++ src/main/java/com/test/javaspringvue/JavaSpringVueApplication.java
@@ -0,0 +1,13 @@
+package com.test.javaspringvue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringVueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringVueApplication.class, args);
+    }
+
+}
